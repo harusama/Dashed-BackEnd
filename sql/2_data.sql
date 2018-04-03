b8d7c3fb55bc276954a802dc1861157acc2087d2
Fernando López Martínez <fer.lopez94@outlook.com>
2018-04-03 02:23:55 -0500

    Insert extra subject

--- sql/2_data.sql
+++ sql/2_data.sql
@@ -1,2 +1,4 @@
 INSERT INTO campus (state, region, district, name) VALUES ('Monterrey', 'Norte', 'District', 'Mty');
+
 INSERT INTO subjects (name) VALUES ('Introduction to physics');
+INSERT INTO subjects (name) VALUES ('Physics II');
