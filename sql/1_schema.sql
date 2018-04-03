0e7a40b582a58f30cfc9d642404c75ba626d50a6
Fernando López Martínez <fer.lopez94@outlook.com>
2018-04-03 13:41:34 -0500

    Modify schema

--- sql/1_schema.sql
+++ sql/1_schema.sql
@@ -31,8 +31,34 @@ CREATE TABLE hashes (
 
 CREATE TABLE subjects (
    id BIGSERIAL PRIMARY KEY,
-   name VARCHAR(100)
+   name TEXT,
+   description TEXT,
 );
+
+CREATE TABLE units (
+   id BIGSERIAL PRIMARY KEY,
+   number INTEGER DEFAULT 0 CONSTRAINT valid_number_number CHECK (number >= 0),
+   name TEXT,
+   description TEXT,
+   subject_id BIGINT REFERENCES subjects(id)
+);
+
+CREATE TABLE chapters (
+   id BIGSERIAL PRIMARY KEY,
+   number INTEGER DEFAULT 0 CONSTRAINT valid_number_number CHECK (number >= 0),
+   name TEXT,
+   description TEXT,
+   unit_id BIGINT REFERENCES units(id)
+);
+
+CREATE TABLE lessons (
+   id BIGSERIAL PRIMARY KEY,
+   number INTEGER DEFAULT 0 CONSTRAINT valid_number_number CHECK (number >= 0),
+   name TEXT,
+   description TEXT,
+   chapter_id BIGINT REFERENCES chapters(id)
+);
+
 
 CREATE TABLE questions (
    id BIGSERIAL PRIMARY KEY,
@@ -41,7 +67,7 @@ CREATE TABLE questions (
    kind INTEGER CONSTRAINT valid_kind_number CHECK (kind >= 1 AND kind <= 9),
    approved INTEGER DEFAULT 0 CONSTRAINT valid_approved_number CHECK (approved >= 0),
    user_id BIGINT REFERENCES users(id),
-   subject_id BIGINT REFERENCES subjects(id)
+   lesson_id BIGINT REFERENCES lessons(id)
 );
 
 CREATE TABLE answers (
